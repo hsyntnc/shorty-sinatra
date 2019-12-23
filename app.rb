fbdae3cc0f4c6198eaf3015efa6773ef84f3a3a6
Hüseyin TUNÇ <huseyin.tunc@bulutfon.com>
2019-12-23 17:23:14 +0300

    Request specs detailed

--- app.rb
+++ app.rb
@@ -7,16 +7,19 @@ require_relative 'config/redis'
 class App < Sinatra::Base
   # Create a shortcode
   post '/shorten' do
+    content_type :json
 
   end
 
   # Shortcode redirection
   get '/:shortcode' do
+    content_type :json
 
   end
 
   # Shortcode stats
   get '/:shortcode/stats' do
+    content_type :json
 
   end
 end
---
+++ spec/requests/shorty_spec.rb
@@ -0,0 +1,76 @@
+require_relative '../spec_helper'
+
+RSpec.describe 'Shorty ' do
+  def app
+    App
+  end
+
+  let(:valid_params) {{ url: 'https://impraise.com', shortcode: 'imprse' }}
+  let(:valid_without_params) {{ url: 'https://impraise.com' }}
+  let(:invalid_params) {{url: 'https://impraise.com' , shortcode: 'an_invalid_shortcode'}}
+
+  describe 'Shortcode generation' do
+    it 'creates a shortcode with a valid url and shortcode given' do
+      post '/shorten', valid_params
+      parsed_body = JSON.parse(last_response.body)
+
+      expect(last_response.status).to eq(201)
+      expect(parsed_body['shortcode']).to eq valid_params[:shortcode]
+      expect(parsed_body.keys).to eq(['shortcode'])
+    end
+
+    it 'creates a shortcode without a shortcode given' do
+      post '/shorten', valid_without_params
+      parsed_body = JSON.parse(last_response.body)
+
+      expect(last_response.status).to eq(201)
+      expect(parsed_body.keys).to eq(%w(shortcode))
+      expect(parsed_body['shortcode'].match("^[0-9a-zA-Z_]{6}$")).not_to eq nil
+    end
+
+    it 'returns error without a url' do
+      post '/shorten'
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'returns an error if the shortcode given is not valid' do
+      post '/shorten', invalid_params
+      expect(last_response.status).to eq(422)
+    end
+
+    it 'returns an error if the shortcode is already in use' do
+      post '/shorten', valid_params
+      expect(last_response.status).to eq(409)
+    end
+  end
+
+  describe 'Shortcode endpoint' do
+    it 'returns 302 if shortcode was found' do
+      get "/#{valid_params[:shortcode]}"
+      expect(last_response.status).to eq(302)
+      expect(last_response.headers['Location']).to eq(valid_params[:url])
+    end
+
+    it 'returns 404 if shortcode was not found' do
+      get "/#{invalid_params[:shortcode]}"
+      expect(last_response.status).to eq(404)
+    end
+  end
+
+  describe 'Shortcode stats' do
+    it 'returns shortcode stats with an existing code' do
+      get "/#{valid_params[:shortcode]}"
+      expect(last_response.status).to eq(302)
+
+      parsed_body = JSON.parse(last_response.body)
+
+      expect(parsed_body.keys).to eq(%w(startDate lastSeenDate redirectCount))
+      expect(parsed_body['shortcode'].match("^[0-9a-zA-Z_]{6}$")).not_to eq nil
+    end
+
+    it 'returns 404 if shortcode was not found' do
+      get "/#{invalid_params[:shortcode]}"
+      expect(last_response.status).to eq(404)
+    end
+  end
+end
